e91783c8c4bc99bd1fb894f6e743f40c9d2e7daa
hsh814 <hsh814@naver.com>
2025-11-19 11:04:10 +0900

    Update Dockerfile

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -2,3 +2,5 @@
 
 git submodule sync && git submodule update --init
 cd solver/fuzzy-sat && git fetch && git submodule sync && git submodule update --init
+
+docker build -t fuzzolic:latest -f docker/fuzzolic-runner/Dockerfile.Ubuntu2004 .
